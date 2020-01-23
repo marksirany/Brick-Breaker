7c77e78324235805fabeb7c2294378b7a26e7369
Mark Sirany <marksirany@me.com>
2020-01-23 17:02:50 -0600

    added code from Brian

--- src/components/Paddle.js
+++ src/components/Paddle.js
@@ -1,6 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Paddle.css";
 
-export default function paddle() {
-  return <div className="paddle" />;
+export default function Paddle() {
+  const [paddleX, setPaddleX] = useState(0);
+  function handleMouse(e) {
+    let boundedX;
+    const offset = (window.innerWidth - 300) / 2;
+    if (e.x - offset < 0) {
+      boundedX = 0;
+    } else if (e.x - offset > 200) {
+      boundedX = 200;
+    } else {
+      boundedX = e.x - offset;
+    }
+    setPaddleX(boundedX);
+  }
+  useEffect(() => {
+    window.addEventListener("mousemove", handleMouse);
+  }, []);
+  return (
+    <div
+      className="paddle"
+      style={{
+        left: `${paddleX}px`
+        // transform: `translateX(calc(-50% + ${paddleX}px))`
+      }}
+    />
+  );
 }
